9a0d64416e7de72125db9b83f12f35d4e97db813
Meenu9050 <mmsharma9050@gmail.com>
2025-09-18 22:14:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 #Get API_KEY from @DeadlineTechOwner or @DeadlineApiBot
 API_BASE_URL = getenv("API_BASE_URL", "")
-#API_KEY = getenv("API_KEY","lOTIJ18790IrWzcpqpxA")
+API_KEY = getenv("API_KEY","lOTIJ18790IrWzcpqpxA")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Zunnu:zunnumusicbot@zunnu.tlsdrgr.mongodb.net/?retryWrites=true&w=majority")
